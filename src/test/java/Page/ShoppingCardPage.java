4161b02a081a49e30ed11b613f4d1455209b35df
Selcuk <selcukemnn@gmail.com>
2024-09-05 21:04:51 +0300

    selcuk: done 27

--- src/test/java/Page/ShoppingCardPage.java
+++ src/test/java/Page/ShoppingCardPage.java
@@ -1,20 +1,78 @@
 package Page;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.devtools.v85.network.model.DataReceived;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
+import utilities.OptionsMet;
+import utilities.ReusableMethods;
+
+import javax.sound.midi.InvalidMidiDataException;
 
 public class ShoppingCardPage {
 	public ShoppingCardPage() {
 		PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()), this);
 	}
 
-	@AndroidFindBy()
-	public WebElement element;
+	//HomePage
+	@AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(18)")
+	public WebElement cardIcon; //Anasayfa card ikonu
 
-	@AndroidFindBy()
-	public WebElement element1;
+	@AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(9)")
+	public WebElement increaseQuantityButton; // + butonu
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(8)")
+	public WebElement decreaseQuantityButton; // - butonu
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Add To Cart\")")
+	public WebElement addToChartButton;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Flower Print Foil T-shirt\n0 (0  Reviews)\n$65.00\")")
+	public WebElement firstProduct;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(10)")
+	public WebElement cardIconOnProductPage;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Success\nProduct added to cart\")")
+	public WebElement toasterMessageTextOnSucces;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Flower Print Foil T-shirt\\nS\\n$65.00\\n1\")")
+	public WebElement firstProductOnCard;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.widget.ImageView\").instance(0)")
+	public WebElement productBackButton;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Remove\")")
+	public WebElement removeProductOnTheCardButton;
+
+	@AndroidFindBy(xpath = "(//*[@index='3'])[1]")
+	public WebElement subTotalText;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Proceed to Checkout\")")
+	public WebElement proceedToCheckoutButton;
+
+	@AndroidFindBy(uiAutomator = "new UiSelector().description(\"Go to Shopping\")")
+	public WebElement goToShoppingButton;
+
+
+	public void addToCard(String size, int quantity) {
+		Driver.getDriver().findElement(AppiumBy.accessibilityId(size)).click();
+
+		for (int i = 0; i < quantity; i++) {
+			increaseQuantityButton.click();
+		}
+
+		try {
+			OptionsMet.swipe(527, 1824, 581, 960);
+			ReusableMethods.wait(1);
+		} catch (InvalidMidiDataException e) {
+			throw new RuntimeException(e);
+		}
+		addToChartButton.click();
+		ReusableMethods.wait(4);
+		productBackButton.click();
+	}
 }
--- src/test/resources/Features/US27_Card.feature
+++ src/test/resources/Features/US27_Card.feature
@@ -4,15 +4,27 @@ Feature: As a registered user,
   Background:
     * User makes driver adjustments
     * User perform sign in with email: "userSelcuk", password: "passSelcuk"
-    * User Navigate to the Dashboard
 
-  @nl
   Scenario: Verify the cart icon is visible and active on the homepage.
+    * User verify that card icon is visible and active
 
   Scenario: Verify products are displayed in the cart and can be deleted.
+    * User add 5 products to card
+    * User clicks to card page
+    * User verify that products are displayed
+    * User delete the product on the card
+    * User verify that product has been deleted
 
   Scenario: Verify the subtotal information is visible on the Shopping Cart page.
+    * User add 5 products to card
+    * User clicks to card page
+    * User verify that subtotal information is visible
 
-  Scenario: Verify the Proceed to Checkout button is visible, active, and redirects to the checkout page.
+  Scenario: Verify the Proceed to Checkout button is visible
+    * User add 5 products to card
+    * User clicks to card page
+    * User verify that proceed to checkout button is visible and active
 
   Scenario: Verify the Go to Shopping button is visible and active if there are no items in the cart.
+    * User clicks to card page
+    * User verify that go to shopping button is visible
